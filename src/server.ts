c4f223405f1bfc28c154d38ea8261009ca3935bd
jardeylson jacinto <jardeylsong.m@gmail.com>
2023-10-26 12:01:05 -0300

    chore: create userRouter and todoRouter

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,11 @@
 import { fastify } from 'fastify'
+import { todoRoutes } from './routes/todoRoutes'
+import { userRoutes } from './routes/userRoutes'
 
 const app = fastify()
+
+app.register(todoRoutes)
+app.register(userRoutes)
 
 app.listen({ port: 3333 }, (err, address) => {
   if (err) {
